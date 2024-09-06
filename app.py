8af175a22c3ff1265e853c6da891dda84bb9c4de
jhig119 <jhig119@wgu.edu>
2024-09-06 11:11:34 -0400

    added javascript to prevent full page reload

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+from flask import Flask, render_template, jsonify
 import random
 
+app = Flask(__name__)
+
+# Define syllable groups
 beginnings = ['An', 'Lo', 'Is', 'Tar', 'Ur', 'Jen', 'Mor', 'Al', 'Sen']
 middles = ['dre', 'fal', 'gor', 'las', 'net', 'rak', 'sav', 'vin', 'zul']
 ends = ['dar', 'ius', 'moth', 'wen', 'thor', 'ion', 'on', 'an', 'el']
 
 def generate_name():
-    # Randomly decide how many syllables the name will have
     syllables_count = random.randint(1, 3)  # 1, 2, or 3 syllables
 
-    # Build the name based on the number of syllables
     if syllables_count == 1:
         return random.choice(beginnings)
     elif syllables_count == 2:
@@ -16,4 +18,15 @@ def generate_name():
     else:
         return random.choice(beginnings) + random.choice(middles) + random.choice(ends)
 
-print(generate_name())
+@app.route('/')
+def home():
+    name = generate_name()
+    return render_template('index.html', name=name)
+
+@app.route('/generate')
+def generate():
+    name = generate_name()
+    return jsonify(name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
